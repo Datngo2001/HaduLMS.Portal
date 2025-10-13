c10eaa0507683abd8f3a9b90d054484512e1ed7f
datngo2001 <ngominhdat110115@gmail.com>
2025-10-13 17:32:58 +0700

    Fix VITE_API_BASE_URL, Fix action name, Fix package

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,13 +10,21 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  define: {
+    "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
+    "process.env.VITE_API_BASE_URL": JSON.stringify(
+      process.env.VITE_API_BASE_URL
+    ),
+    "process.env.APP_NAME": JSON.stringify(process.env.APP_NAME),
+    "process.env.APP_VERSION": JSON.stringify(process.env.APP_VERSION),
+  },
   server: {
     port: 5173,
     proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
+      "/api": {
+        target: "http://localhost:3001",
         changeOrigin: true,
       },
     },
   },
-})
+});
